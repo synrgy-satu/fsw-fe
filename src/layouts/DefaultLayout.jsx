777ad136efc2b9bca19bb7ccc6e44c2173147cef
Wahyu Pambudi <wahyupambudi823@gmail.com>
2024-08-05 23:52:44 +0700

    feat - authstate navigate

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -4,7 +4,6 @@ import { useLocation } from "react-router-dom";
 import Sidebar from "../components/userPortal/Sidebar";
 import Header from "../components/userPortal/Header";
 import HeaderMutation from "../components/mutation/HeaderMutation";
-import CategoryTransfer from "../components/transfer/CategoryTransfer";
 
 const DefaultLayout = ({ children }) => {
   const location = useLocation();
