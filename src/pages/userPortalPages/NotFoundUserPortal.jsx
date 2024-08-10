4c18f249e8eb3ecb7b1152d67105ffbcbead1c14
20106050083 <agungrashif009@gmail.com>
2024-08-10 13:24:36 +0700

    feat: add not found user portal page

---
+++ src/pages/userPortalPages/NotFoundUserPortal.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const NotFoundUserPortal = () => (
+  <div className="flex flex-col items-center justify-center h-screen">
+    <img src="/images/Ilustarsi 404.png" className="" alt="Ilustarsi 404" />
+    <h2 className="font-bold text-5xl text-center text-[#333999] mb-3">
+      Halaman Tidak Ditemukan!
+    </h2>
+    <p className="font-normal text-xl text-center text-[#1A1A1A] mb-10">
+      Halaman yang kamu cari saat ini tidak tersedia
+    </p>
+    <Link
+      to="/portal"
+      className="w-[329.63px] text-white bg-[#333999] hover:bg-[#272D87] font-bold text-base rounded-lg px-5 py-3 text-center"
+    >
+      Kembali ke Beranda
+    </Link>
+  </div>
+);
+
+export default NotFoundUserPortal;
