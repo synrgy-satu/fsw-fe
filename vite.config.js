a9c9f9578dec919e36b83577893901081954215c
dannyysaputra <84920467+dannyysaputra@users.noreply.github.com>
2024-08-16 00:47:29 +0700

    feat: change response api

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '192.168.1.12'
+    host: true // Accept connections on all network interfaces
   }
 })
